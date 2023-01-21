4db89332640bc8a16f212d96508a391f72434f24
hiki7 <rsaliyev03@gmail.com>
2023-01-21 21:18:06 +0500

    11th task

---
+++ 11.py
@@ -0,0 +1,20 @@
+x = "Hello"
+y = 16
+
+print(bool(x))
+print(bool(y))
+
+#functions
+def myFunction() :
+  return True
+
+print(myFunction())
+
+#yes/no
+def myFun() :
+  return True
+
+if myFun():
+  print("Yes")
+else:
+  print("No")
