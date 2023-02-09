0a6e0b114a578644e9579ea475edeb1501ee3897
hiki7 <rsaliyev03@gmail.com>
2023-02-09 23:49:23 +0500

    My commit

---
+++ tsis3/functions2/5.py
@@ -0,0 +1,94 @@
+def av_cat(movies):
+    categ = []
+    for x in range(0, len(movies)-1):
+        if movies[x]["category"] not in categ:
+            categ.append(movies[x]["category"])
+    
+    summ = float(0)
+    count = float(0)
+    for i in range(0, len(categ)-1):
+        for x in range(0, len(movies)-1):
+            if categ[i] == movies[x]["category"]:
+                summ += movies[x]["imdb"]
+                count += 1
+        print(categ[i], summ / count)
+
+
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+av_cat(movies)
